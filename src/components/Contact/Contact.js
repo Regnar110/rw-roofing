be29940f36a6b10bcfd73a4385fab880876e1ea1
unknown <mateuszwrycza3@gmail.com>
2020-12-31 17:54:02 +0100

    contact details

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,8 +5,16 @@ const Contact = () => {
     return (
         <div className='contact'>
             <h1>Kontakt</h1>
-            
-        </div>
+            <div className='contact-content'>
+                <div class='contact-details'>123</div>
+                <div class="mapouter">
+                    <div class="gmap_canvas">
+                        <iframe width="602" height="344" id="gmap_canvas" src="https://maps.google.com/maps?q=starogard%20GDa%C5%84ski%2C%20ul%20kleberga%2074a&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" />
+                        <a href="https://123movies-to.org">deadpool 123movies</a>
+                    </div>
+                </div>
+            </div>
+        </div>  
     )
 }
 
